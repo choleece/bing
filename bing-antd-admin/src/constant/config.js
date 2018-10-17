85652891b7cab37e53eb0a844630039b40e0c4f4
choleece <choleece@163.com>
2018-10-17 23:39:34 +0800

    完善jwt util

---
+++ bing-antd-admin/src/constant/config.js
@@ -0,0 +1,6 @@
+export default {
+  return_code: {
+    success: 0,
+    fail: -1,
+  },
+};
---
+++ bing-common/src/main/java/cn/choleece/bing/common/vo/LoginUser.java
@@ -0,0 +1,7 @@
+package cn.choleece.bing.common.vo;
+
+/**
+ * Created by choleece on 2018/10/17.
+ */
+public class LoginUser {
+}
