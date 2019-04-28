1fbe97746bf5890ce349f6ce80a64d469bedd355
choleece <choleece@163.com>
2019-04-28 23:32:54 +0800

    加入bing-ums用户管理

---
+++ bing-ums/src/main/java/cn/choleece/bing/ums/BingUmsApplication.java
@@ -0,0 +1,13 @@
+package cn.choleece.bing.ums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingUmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingUmsApplication.class, args);
+	}
+
+}
